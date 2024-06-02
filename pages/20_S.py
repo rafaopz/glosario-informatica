61df1641a59934fbf9c41d63e7c10484128a9ad1
rafaopz <rafael415opz@gmail.com>
2024-06-01 21:54:47 -0400

    agrego terminos

--- pages/20_S.py
+++ pages/20_S.py
@@ -7,3 +7,6 @@ st.markdown('***') # linea separadora
 
 st.write('**Software:** Estos son los programas informáticos que hacen posible la ejecución de tareas específicas dentro de un computador. Por ejemplo, los sistemas operativos, aplicaciones, navegadores web, juegos o programas. Estas características siempre trabajan de la mano.')
 st.markdown('***') # linea separadora
+
+st.write('**SQL:** ')
+st.markdown('***') # linea separadora
